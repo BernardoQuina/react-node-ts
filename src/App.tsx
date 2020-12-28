3fd7b0dd194a95e835428971ce91b6254f612c83
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-28 16:35:42 +0000

    Working & managing state & types

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import TodoList from './components/TodoList'
 import NewTodo from './components/NewTodo'
 
+import { Todo } from './todo.model'
+
 const App = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }]
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const todoAddHandler = (text: string) => {
-    console.log(text)
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ])
+  }
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   )
 }
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -11,12 +11,18 @@ const NewTodo: React.FC<NewTodoProps> = ({ onAddTodo }) => {
     event.preventDefault()
     const enteredText = textInputRef.current!.value
     onAddTodo(enteredText)
+    textInputRef.current!.value = ''
   }
   return (
     <form onSubmit={todoSubmitHandler}>
       <div>
         <label htmlFor='todo-text'>Todo Text</label>
-        <input type='text' id='todo-text' ref={textInputRef} />
+        <input
+          placeholder=' ... goes here'
+          type='text'
+          id='todo-text'
+          ref={textInputRef}
+        />
       </div>
       <button type='submit'>ADD TODO</button>
     </form>
