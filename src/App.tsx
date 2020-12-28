b577302438101fb00ad84ca6ccb29e9fe108f2b0
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-28 15:46:04 +0000

    Working with props and types for props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-function App() {
+import TodoList from './components/TodoList'
+
+const App = () => {
+  const todos = [{ id: 't1', text: 'Finish the course' }]
   return (
     <div className="App">
+      {/* A component that adds todos */}
+      <TodoList items={todos} />
     </div>
   )
 }
